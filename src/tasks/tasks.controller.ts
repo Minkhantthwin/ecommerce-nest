266fa5b878601056f1c5e49bfafa30239ea1bea4
Minkhantthwin <minkhantthwin17@gmail.com>
2025-10-17 16:33:37 +0630

    #1 | feat: prisma schema added

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('tasks')
+export class TasksController {
+
+    @Get()
+    public getTasks(): string[] {
+        return ['Task 1', 'Task 2', 'Task 3'];
+    }
+}
